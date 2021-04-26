160193f5cf387ef297a0b38ecd72e24b9da3d425
Mateus Couto <42397879+MateusCouto@users.noreply.github.com>
2021-04-25 22:27:06 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Devices/ComboDevice.cs
@@ -0,0 +1,22 @@
+﻿using Course.Devices;
+using System;
+
+namespace Course.Entities
+{
+    class ComboDevice: Device, IScanner, IPrinter
+    {
+        public override void ProcessDoc(string document)
+        {
+            Console.WriteLine("ComboDevice processing " + document);
+        }
+        public void Print(string document)
+        {
+            Console.WriteLine("ComboDevice print " + document);
+        }
+
+        public string Scan()
+        {
+            return "ComboDevice Scan result";
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,24 @@
+﻿using Course.Entities;
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p = new() { SerialNumber = 1080 };
+            p.ProcessDoc("My Letter");
+            p.Print("My Letter");
+
+            Scanner s = new() { SerialNumber = 2003 };
+            s.ProcessDoc("My Email");
+            Console.WriteLine(s.Scan());
+
+            ComboDevice c = new() { SerialNumber = 3921 };
+            c.ProcessDoc("My dissertation");
+            c.Print("My dissertation");
+            Console.WriteLine(c.Scan());
+        }
+    }
+}
